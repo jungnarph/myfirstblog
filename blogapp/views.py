0d3d331b4eddea93f3e0993b3faf3fb41f054988
Exulten <stem.danieljohnbucad@gmail.com>
2025-02-28 19:24:25 +0800

    TUTORIAL EMAIL

--- blogapp/views.py
+++ blogapp/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm
+    return render(request, 'blogapp/post/share.html', {'post': post, 'form': form})
 
 def post_list(request):
     object_list = Post.published.all()
