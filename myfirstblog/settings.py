bacf73bac3e8abc0265481c72b2301442f679e57
Daniel John Bucad <stem.danieljohnbucad@gmail.com>
2025-03-17 17:54:46 +0800

    until page 17

--- myfirstblog/settings.py
+++ myfirstblog/settings.py
@@ -27,10 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
